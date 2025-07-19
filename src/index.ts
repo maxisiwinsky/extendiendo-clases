8b009ebc2b4f2749b657cabcdad9387673be5381
maxisiwinsky <maxisiwinsky15@gmail.com>
2025-07-19 01:58:56 +0000

    cree la clase listaDeProductos y sus metodos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import product from "./products.json"
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +25,46 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  products: Product[];
+  constructor(name:string){
+    super(name);    
+    this.products = [];
+    product.forEach((p=>this.addProduct(p)));
+}
 
+addProduct(product: Product): void {
+  // Verificar si ya existe un producto con el mismo ID
+  const exists = this.products.filter(p => p.id === product.id).length > 0;
+
+  if (!exists) {
+    // Si no existe, agregar el producto
+    this.products.push(product);
+    this.add(product);
+  }
+}
+
+getProduct(id:number): Product
+{
+  return this.products.find(p=>p.id===id);
+}
+
+removeProduct(id:number): void
+{
+  const index = this.products.findIndex(p => p.id === id);
+if (index !== -1) {
+  this.products.splice(index, 1);
+}
+}
+
+ getSortedByPrice(order: string): Product[] {
+  const copy = this.products;
+      if(order==="desc"){
+        return copy.sort((a,b)=>b.price-a.price);
+      }
+      else if(order==="asc"){
+        return copy.sort((a,b)=>a.price - b.price);
+      }
+}
+}
 export { ListaDeProductos, Product };
